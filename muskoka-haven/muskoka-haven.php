41bf4148ea5818d37c9859de65b22a7ce3e32cad
jacobdaub <jacob.daub@gmail.com>
2022-09-17 21:03:39 -0400

    1st commit

---
+++ muskoka-haven/muskoka-haven.php
@@ -0,0 +1,17 @@
+<?php
+include('../menu.php');
+?>
+<div class="text-content">
+  <h3>Muskoka Haven</h3>
+  <img src="../pics/lowres/mh/deck-view.jpg"
+  style = "width: 800px; height:scale;">
+  <p>Muskoka Haven is our newly renovated to cottage on Gloucester Pool.</p>
+  <p></p>
+  <h3>Key Info</h3>
+  <ul>
+    <li>Accomodates 9 adults</li>
+    <li>Rent weekends or entire weeks</li>
+    <li>...</li>
+    <li>Don't think, book now!</li>
+  </ul>
+</div>
