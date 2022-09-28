8b7cae58d32d8ffcdbb280e9d25b01e7dff6101f
jacobdaub <jacob.daub@gmail.com>
2022-09-27 21:57:59 -0400

    Version 0.3
    Cleaned up the images and got rid of broken links.

--- homepage.php
+++ homepage.php
@@ -8,27 +8,25 @@
   <div class="main-title">
     <h1 id ="highlight">Muskoka Cabins on the Pool</h1>
   </div>
-     <p>Welcome! We have three cottages available for rent on Gloucester Pool Lake. They are Muskoka Haven, Blueberry, and Wildflower. We have availability going into the fall and we are taking bookings for next year. </p>
-     <p> Please contact Jacob for bookings at cottagerentalontario@gmail.com or call (416) 268-0396. </p>
-    
-     <!--creating a moasic of pictures--> 
-    <div class="grid-container"> 
-      <div class="muskoka-haven">
-        <a href="./muskoka-haven/muskoka-haven.php">
-          <h2>Muskoka Haven</h2>
-          <img class = "fill-image" src = "pics/lowres/mh/master-bedroom-1.jpg" alt ="Muskoka Haven Master Bedroom">
-        </a>
-      </div>
-      <div class ="wildflower">
-        <a href="./blueberry/home.php">
-          <h2>Wildflower</h2>
-          <img class = "half-fill" src="pics/lowres/wf/main.jpg" >
-        </a>
-      </div> 
-      <div class ="blueberry">
-        <a href="./blueberry/blueberry.php">
+  <p>Welcome! We have three cottages available for rent on Gloucester Pool Lake. They are Muskoka Haven, Blueberry, and Wildflower. We have availability going into the fall and we are taking bookings for next year. </p>
+  <p> Please contact Jacob for bookings at cottagerentalontario@gmail.com or call (416) 268-0396. </p> 
+  <div class = "image-border">
+    <a href="./muskoka-haven/muskoka-haven.php">
+      <h2>Muskoka Haven</h2>
+      <img style = "width : 100%;" src = "pics/mh/master-bedroom-1.jpg" alt ="Muskoka Haven Master Bedroom">
+    </a>
+  </div>
+  <div class ="double-photo">
+    <div class = "column-left">
+      <a href="https://www.cottagesincanada.com/22294">
+        <h2>Wildflower</h2>
+        <img style = "width: 100%"; src="pics/wf/main.jpg">
+      </a>
+    </div> 
+      <div class ="column-right">
+        <a href="https://www.cottagesincanada.com/10472">
           <h2>Blueberry</h2>
-          <img class = "half-fill"  src="pics/lowres/bb/outside.jpeg" >
+          <img style = "width: 100%;"  src="pics/bb/outside.jpeg">
         </a>
       </div>
     </div>
