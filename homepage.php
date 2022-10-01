20548bbad26be7b12f6eb6810e7cc5b29806cb33
jacobdaub <jacob.daub@gmail.com>
2022-09-30 22:34:45 -0400

    0.4
    Wondering why there is a margin on my double pair of photos in muskoka-haven.php/
    Appears to be a bottom margin and cant figure out why.

--- homepage.php
+++ homepage.php
@@ -1,8 +1,8 @@
 <html>
 
 <head>
-<link rel="stylesheet" type="text/css" href="style/general.css">
-<link rel="stylesheet" type="text/css" href="style/homepage.css">
+<link rel="stylesheet" type="text/css" href="style/general.css?v.1">
+<link rel="stylesheet" type="text/css" href="style/homepage.css?v.1">
 </head>
 <div class ="set-width">
   <div class="main-title">
@@ -10,25 +10,31 @@
   </div>
   <p>Welcome! We have three cottages available for rent on Gloucester Pool Lake. They are Muskoka Haven, Blueberry, and Wildflower. We have availability going into the fall and we are taking bookings for next year. </p>
   <p> Please contact Jacob for bookings at cottagerentalontario@gmail.com or call (416) 268-0396. </p> 
-  <div class = "image-border">
-    <a href="./muskoka-haven/muskoka-haven.php">
-      <h2>Muskoka Haven</h2>
-      <img style = "width : 100%;" src = "pics/mh/master-bedroom-1.jpg" alt ="Muskoka Haven Master Bedroom">
-    </a>
-  </div>
-  <div class ="double-photo">
-    <div class = "column-left">
+
+
+  <a href="./muskoka-haven/muskoka-haven.php">
+    <h2>Muskoka Haven</h2>
+    <div class = "feature-cottage">
+      <img class = "image-border" src = "pics/mh/master-bedroom-1.jpg" alt ="Muskoka Haven Master Bedroom">
+    </div>
+  </a>
+  <div class = "inline">
+    <div class = "half-width">
       <a href="https://www.cottagesincanada.com/22294">
         <h2>Wildflower</h2>
-        <img style = "width: 100%"; src="pics/wf/main.jpg">
+        <div class ="feature-cottage">
+          <img class = "image-border" src="pics/wf/main.jpg">
+        </div>
       </a>
-    </div> 
-      <div class ="column-right">
-        <a href="https://www.cottagesincanada.com/10472">
-          <h2>Blueberry</h2>
-          <img style = "width: 100%;"  src="pics/bb/outside.jpeg">
-        </a>
-      </div>
+    </div>
+  <div class ="half-width">
+      <a href="https://www.cottagesincanada.com/10472">
+        <h2>Blueberry</h2>
+        <div class = "feature-cottage">
+          <img class = "image-border" src="pics/bb/outside.jpeg">
+        </div>
+      </a>
     </div>
   </div>
+</div>
 </html>
--- muskoka-haven/muskoka-haven.php
+++ muskoka-haven/muskoka-haven.php
@@ -7,9 +7,8 @@ include('../menu.php');
 ?> 
 <div class = "set-width">
   <h2>  Muskoka Haven</h2>
-  <div class = "image-border">
-    <img src="../pics/mh/deck-view.jpg"
-  style = "width: 100%; ">
+  <div class = "feature-cottage">
+    <img class = "image-border" src="../pics/mh/deck-view.jpg">
   </div>
   <p>Muskoka Haven is an original cottage built in 1972 on the east side of Gloucester Pool. It joins together rustic charm with modern convenience through its comfortable bedrooms and newly renovated kitchen and bathrooms. Built 40 feet up high on the Canadian Shield, Muskoka Haven has some of the best views on the lake.</p>
 
@@ -34,43 +33,62 @@ include('../menu.php');
   The downstairs deck bedroom. Features a queen bed, and a sliding door facing the back of the cottage. This bedroom gets the morning sun. The bedroom also has a dresser, two mirrors, a nightstand and ceiling fan. 
   </p> 
 
-  <div class = "double-photo">
-    <div class = "column-left">
-      <img src = "../pics/mh/deck-bedroom-1.jpg" style = "width:100%">
+  <div class = "inline">
+    <div class = "half-width">
+      <div class = "feature-cottage">
+        <img class ="image-border" src = "../pics/mh/deck-bedroom-1.jpg">
+      </div>
     </div>
-    <div class = "column-right">
-      <img src = "../pics/mh/deck-bedroom-2.jpg"style = "width:100%">
+    <div class = "half-width">
+      <div class = "feature-cottage">
+        <img class = "image-border" src = "../pics/mh/deck-bedroom-2.jpg">
+      </div>
     </div>
   </div>
+
   <p>The east wing bedroom. Here you will feel like you are in your own world. The walls are stained with raw organic linseed oil giving the room a pleasant smell. This bedroom has a queen bed, a remote controlled ceiling fan, two mirrors and a dresser.
   </p>
-  <div class = "image-border">
-    <img style = "  width: 100%;" src = "../pics/mh/linseed-bedroom.jpg">
+
+  <div class = "feature-cottage">
+    <img class = "image-border" src = "../pics/mh/linseed-bedroom.jpg">
   </div>
   <p> The kids bedroom is upstairs. The room has two single beds and an option to pull out a third. A small bathroom connects the kids bedroom and the master bedroom. The room has high ceilings, big windows, a ceiling fan, dresser, and mirror. </p>
-  <div class = "double-photo">
-    <div class = "column-left">
-      <img src = "../pics/mh/kids-bedroom-1.jpg" style = "width:100%">
+
+  <div class = "inline">
+    <div class = "half-width">
+      <div class = "feature-cottage">
+        <img class = "image-border" src = "../pics/mh/kids-bedroom-1.jpg">
+      </div>
     </div>
-    <div class = "column-right">
-     <img src = "../pics/mh/kids-bedroom-2.jpg"style = "width:100%">
+    <div class = "half-width">
+      <div class = "feature-cottage">
+       <img class= "image-border" src = "../pics/mh/kids-bedroom-2.jpg">
+      </div>
     </div>
   </div>
 
 
   <p> The master bedroom is located upstairs facing the water. It has its own private deck with seating where you can enjoy the sunset with your favourite drink. On the deck there is a small cooler. Also in the room is an HDTV with a bluray player and a collection of movies.</p>
-  <div class = "image-border">
-  <img style = "  width: 100%;" src = "../pics/mh/master-bedroom-1.jpg">
+
+
+  <div class = "inline">
+    <div class = "half-width">
+      <div class = "feature-cottage">
+        <img class = "image-border" src = "../pics/mh/master-bedroom-2.jpg">
+      </div>
+    </div>
+
+    <div class = "half-width">
+      <div class = "feature-cottage">
+        <img class = "image-border" src = "../pics/mh/master-bedroom-3.jpg">
+      </div>
+    </div>
   </div>
-  <div class = "double-photo">
-    <div class = "column-left">
-      <img style = "width:100%" src = "../pics/mh/master-bedroom-2.jpg">
-    </div>
-    <div class = "column-right">
 
-      <img style = "width:100%;" src = "../pics/mh/master-bedroom-3.jpg">
-    </div>
-
+    <div class = "feature-cottage">
+    <img class = "image-border" src = "../pics/mh/master-bedroom-1.jpg">
+  </div>
+  
 <!--
   <h3>Kitchen</h3>
   //<p>The kitchen has
